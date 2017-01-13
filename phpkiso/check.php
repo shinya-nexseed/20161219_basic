2b2fd50e257754a9fc3880c7ea28c36ecbe367cb
shinya-nexseed <shirai@nexseed.net>
2017-01-13 12:56:24 +0800

    アンケートフォーム作成②

--- phpkiso/check.php
+++ phpkiso/check.php
@@ -11,9 +11,39 @@
     // $_POST = array(入力タグのname => 入力された値)
 
     // $_POST = array('nickname' => 'shinya', 'email' => 'shirai@nexseed.net', 'content' => 'ほげほげ');
-    $nickname = $_POST['nickname'];
-    $email = $_POST['email'];
-    $content = $_POST['content'];
+    $nickname = htmlspecialchars($_POST['nickname']);
+    $email = htmlspecialchars($_POST['email']);
+    $content = htmlspecialchars($_POST['content']);
+
+    // バリデーション (入力チェック)
+    $nickname_result = ''; // 変数の初期化
+    if ($nickname == '') {
+        // $nicknameが空だった場合の処理
+        $nickname_result = 'ニックネームが入力されていません。';
+    } else {
+        // 正しく入力された場合の処理
+        $nickname_result = 'ようこそ' . $nickname . '様';
+    }
+
+    $email_result = ''; // 変数の初期化
+    if ($email == '') {
+        // $emailが空だった場合の処理
+        $email_result = 'メールアドレスが入力されていません。';
+    } else {
+        // 正しく入力された場合の処理
+        $email_result = 'メールアドレス : ' . $email;
+    }
+
+    $content_result = ''; // 変数の初期化
+    if ($content == '') {
+        // $contentが空だった場合の処理
+        $content_result = 'お問い合わせ内容が入力されていません。';
+    } else {
+        // 正しく入力された場合の処理
+        $content_result = 'お問い合わせ内容 : ' . $content;
+    }
+
+
  ?>
 
 <!DOCTYPE html>
@@ -24,17 +54,27 @@
 </head>
 <body>
   <h1>入力内容確認</h1>
-  <?php echo 'ようこそ' . $nickname . '様'; ?>
+  <?php echo $nickname_result; ?>
   <br>
-  <?php echo 'メールアドレス : ' . $email; ?>
+  <?php echo $email_result; ?>
   <br>
-  <?php echo 'お問合わせ内容 : ' . $content; ?>
+  <?php echo $content_result; ?>
   <br>
   <form method="POST" action="thanks.php">
     <input type="hidden" name="nickname" value="<?php echo $nickname ?>">
     <input type="hidden" name="email" value="<?php echo $email ?>">
     <input type="hidden" name="content" value="<?php echo $content ?>">
-    <input type="submit" value="OK">
+    <a href="index.html" onclick="history.back()">戻る</a>
+    <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+      <input type="submit" value="OK">
+    <?php endif; ?>
+
+    <?php
+        // if (条件) {
+        //     echo '<input type="submit" value="OK">';
+        // }
+     ?>
+
   </form>
 </body>
 </html>
--- phpkiso/thanks.php
+++ phpkiso/thanks.php
@@ -1,7 +1,30 @@
 <?php
-  $nickname = $_POST['nickname'];
-  $email = $_POST['email'];
-  $content = $_POST['content'];
+    $nickname = htmlspecialchars($_POST['nickname']);
+    $email = htmlspecialchars($_POST['email']);
+    $content = htmlspecialchars($_POST['content']);
+
+    // 1. データベースへ接続する
+    $dsn = 'mysql:dbname=phpkiso;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+
+    // 2. SQL文を実行する
+    $sql = 'INSERT INTO `survey` (`nickname`, `email`, `content`) VALUES
+    ("'.$nickname.'", "'.$email.'", "'.$content.'")';
+    // アクサングラーブ shift + @
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    // 3. データベースを切断する
+    $dbh = null;
+
+    // CRUD処理とSQL文
+    // Create : INSERT
+    // Read   : SELECT
+    // Update : UPDATE
+    // Delete : DELETE
 ?>
 <!DOCTYPE html>
 <html lang="ja">
