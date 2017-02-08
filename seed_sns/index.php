c6e8dff6530f1bd092962b3e08a31cdd134748e6
shinya-nexseed <shirai@nexseed.net>
2017-02-08 14:23:29 +0800

    ツイート登録・表示処理

--- seed_sns/index.php
+++ seed_sns/index.php
@@ -1,15 +1,18 @@
 <?php
 session_start();
 require('dbconnect.php');
-echo $_SESSION['id'];
-echo '<br>';
-echo $_SESSION['time'];
+// echo $_SESSION['id'];
+// echo '<br>';
+// echo $_SESSION['time'];
 
 // ログイン状態のチェック
 if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
     // ログイン状態の定義
       // $_SESSION['id']が存在する
       // $_SESSION['time']が現在時刻の1時間 (3600秒) 以内である
+
+    // ユーザーがアクションするごとに (ページがリロードされるごとに) 時間を更新
+    $_SESSION['time'] = time();
 
     // ログインしていればログインユーザー情報をDBから取得
     $sql = 'SELECT * FROM `members` WHERE `member_id`=?';
@@ -21,7 +24,33 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
     $login_member = $stmt->fetch(PDO::FETCH_ASSOC);
 } else {
     // ログインせずにページを訪れた場合はlogin.phpへ遷移
+    header('Location: login.php');
+    exit();
 }
+
+// ツイートボタンが押されたとき
+if (!empty($_POST)) {
+    if ($_POST['tweet'] != '') {
+        // tweetsテーブルに投稿データを登録 : Create - INSERT
+        $sql = 'INSERT INTO `tweets` SET `tweet`=?,
+                                          `member_id`=?,
+                                          `created`=NOW()';
+        $data = array($_POST['tweet'], $_SESSION['id']);
+
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
+        // データ登録後一度画面をリロードしPOST送信をリセットする
+        header('Location: index.php');
+        exit();
+    }
+}
+
+// ツイートデータをデータベースから取得
+$sql = 'SELECT t.*, m.nick_name, m.picture_path FROM `tweets` AS t, `members` AS m WHERE m.member_id = t.member_id ORDER BY t.created DESC';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
 ?>
 
 <!DOCTYPE html>
@@ -31,8 +60,32 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
   <title></title>
 </head>
 <body>
-  <?php echo $login_member['nick_name']; ?>
-  <img src="member_picture/<?php echo $login_member['picture_path']; ?>" width="100">
+  <?php echo $login_member['nick_name']; ?><br>
+  <img src="member_picture/<?php echo $login_member['picture_path']; ?>" width="100"><br>
+
+  <br>
+
+  <form method="POST" action="index.php">
+    <div>
+      <textarea name="tweet" placeholder="つぶやき" cols="50" rows="5"></textarea>
+    </div>
+    <input type="submit" value="ツイート">
+  </form>
+
+  <?php while($tweet = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+    id : <?php echo $tweet['tweet_id']; ?> - <img src="member_picture/<?php echo $tweet['picture_path']; ?>" style="width: 16px;"><?php echo $tweet['tweet']; ?> <span style="font-size: 14px; color: #808080;"><?php echo $tweet['created']; ?> (<?php echo $tweet['nick_name']; ?>)</span><br>
+  <?php endwhile; ?>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
---
+++ seed_sns/sql_template.php
@@ -0,0 +1,47 @@
+<?php
+// INSERT文
+
+// ①INSERT INTO `テーブル名` (`カラム1`, `カラム2`, `カラム3`) VALUES ("値1", "値2", "値3")
+// ②INSERT INTO `テーブル名` SET `カラム1`="値1", `カラム2`="値2", `カラム3`="値3"
+
+// SELECT文
+// 複数条件付き
+// SELECT * FROM `テーブル名` WHERE 条件1 AND 条件2
+
+// テーブルのリレーションシップ
+// MySQL → RDB (Relational Database)
+// 一回のsql文で複数テーブルから条件を元に関連するデータを取得する手法
+// SELECT * FROM テーブルA, テーブルB WHERE 関連条件
+// SELECT * FROM `tweets`, `members` WHERE `members`.member_id = `tweets`.member_id
+
+// テーブルの結合
+// 外部結合 : LEFT JOIN
+// SELECT * FROM テーブルA LEFT JOIN テーブルB
+// 左にあるテーブルが主データとなり、関連する情報を右テーブルから結合させる
+
+// AS句
+// A AS a
+// tweets AS t, members AS m
+// SELECT * FROM `tweets` AS t, `members` AS m WHERE m.member_id = t.member_id ORDER BY t.created DESC
+
+// カラムの指定
+// SELECT * FROM `tweets` WHERE `tweet_id`=1
+// アスタリスク * → ワイルドカード = 全部
+// SELECT カラム1, カラム2 FROM `テーブル`
+// SELECT t.*, m.nick_name, m.picture_path FROM `tweets` AS t, `members` AS m WHERE m.member_id = t.member_id ORDER BY t.created DESC
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
